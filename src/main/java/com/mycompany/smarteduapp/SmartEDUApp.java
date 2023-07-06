53601ad93384903d4bd1d3fae2e85669af5e5442
erw9695 <erw9695@nyu.edu>
2023-07-06 15:11:26 -0400

    Upload.

---
+++ src/main/java/com/mycompany/smarteduapp/SmartEDUApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.smarteduapp;
+
+/**
+ *
+ * @author ethan
+ */
+public class SmartEDUApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
